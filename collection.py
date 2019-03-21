56829f1f5fe424367ad4180441a628674d4aef2c
PUGE <100284685@qq.com>
2019-03-21 21:37:19 +0800

    定时任务

--- collection.py
+++ collection.py
@@ -6,12 +6,7 @@ import os
 import json
 import requests
 import time
-
-from flask import Flask
-from flask_cors import *
-
-app = Flask(__name__)
-CORS(app, supports_credentials=True)
+import schedule
 
 
 # 如果不存在目录则创建目录
@@ -34,48 +29,74 @@ def mkdir(path):
   else:
     return False
 
-# 今天日期
-date = time.strftime('%Y.%m.%d',time.localtime(time.time()))
 
-mkdir('./history/' + date)
-
-szListFile = open('sz.json', 'r', encoding='utf-8').read()
-szList = json.loads(szListFile)
-
-
-
-
-while True:
-  print('开始获取!')
-  summary = '股票名字,代码,当前价格,昨日收盘价,今日开盘价,成交的股票数,外盘,内盘,买一报价,买一数量,买二报价,买二数量,买三报价,买三数量,买四报价,买四数量,买五报价,买五数量,卖一报价,卖一数量,卖二报价,卖二数量,卖三报价,卖三数量,卖四报价,卖四数量,卖五报价,卖五数量,最近逐笔成交,时间,涨跌,涨跌%,今日最高价,今日最低价,价格/成交量(手)/成交额,成交量(手),成交金额,换手率(%),市盈率,最高,最低,振幅(%),流通市值(亿),总市值(亿),市净率,涨停价,跌停价,量比,委差,平均成本,PE(动)\n'
-  # 每50个提交一次
-  tempList = []
-  timeData = ''
-  def getData ():
-    listString = ','.join(tempList)
-    data = requests.get(url='http://qt.gtimg.cn/q=' + listString).text
-    sharesList = re.findall(r"=\"1\~(.+?)\";", data)
-    
-    # 去掉空数据
-    for sz in sharesList:
-      global summary
-      sz = sz.replace("~~", "~")
-      global timeData
-      if (timeData == ''):
-        timeData = sz.split('~')[29]
+keepRun = False
+summary = ''
+timeData = ''
+def run():
+  # 今天日期
+  date = time.strftime('%Y.%m.%d',time.localtime(time.time()))
+  mkdir('./history/' + date)
+  szListFile = open('sz.json', 'r', encoding='utf-8').read()
+  szList = json.loads(szListFile)
+  
+  global keepRun
+  while keepRun:
+    global summary
+    global timeData
+    summary = '股票名字,代码,当前价格,昨日收盘价,今日开盘价,成交的股票数,外盘,内盘,买一报价,买一数量,买二报价,买二数量,买三报价,买三数量,买四报价,买四数量,买五报价,买五数量,卖一报价,卖一数量,卖二报价,卖二数量,卖三报价,卖三数量,卖四报价,卖四数量,卖五报价,卖五数量,最近逐笔成交,时间,涨跌,涨跌%,今日最高价,今日最低价,价格/成交量(手)/成交额,成交量(手),成交金额,换手率(%),市盈率,最高,最低,振幅(%),流通市值(亿),总市值(亿),市净率,涨停价,跌停价,量比,委差,平均成本,PE(动)\n'
+    # 每50个提交一次
+    tempList = []
+    timeData = ''
+    def getData ():
+      listString = ','.join(tempList)
+      data = requests.get(url='http://qt.gtimg.cn/q=' + listString).text
+      sharesList = re.findall(r"=\"1\~(.+?)\";", data)
       
-      summary += sz.replace("~",",") + '\n'
+      # 去掉空数据
+      for sz in sharesList:
+        global summary
+        sz = sz.replace("~~", "~")
+        global timeData
+        if (timeData == ''):
+          timeData = sz.split('~')[29]
+        
+        summary += sz.replace("~",",") + '\n'
 
-  for sz in szList:
-    if len(tempList) >= 50:
-      getData()
-      tempList = []
-    tempList.append(sz['id'])
-  # 处理尾巴
-  getData()
-  file_path = './history/' + date + '/' + timeData + '.csv'
-  with open(file_path, mode='w', encoding='utf_8_sig') as file_obj:
-    file_obj.write(summary)
-  print('5秒后重新获取!')
-  # 每5秒执行一次
-  time.sleep(5)
+    for sz in szList:
+      if len(tempList) >= 50:
+        getData()
+        tempList = []
+      tempList.append(sz['id'])
+    # 处理尾巴
+    getData()
+    file_path = './history/' + date + '/' + timeData + '.csv'
+    with open(file_path, mode='w', encoding='utf_8_sig') as file_obj:
+      file_obj.write(summary)
+    # 每5秒执行一次
+    schedule.run_pending()
+    time.sleep(5)
+
+def start():
+  global keepRun
+  if (not keepRun):
+    print('开始运行')
+    keepRun = True
+    run()
+
+def stop():
+  global keepRun
+  if (keepRun):
+    print('停止运行')
+    keepRun = False
+
+
+schedule.every().day.at("09:25").do(start)
+schedule.every().day.at("11:35").do(stop)
+schedule.every().day.at("13:25").do(start)
+schedule.every().day.at("15:35").do(stop)
+
+# 不断运行
+while True:
+  schedule.run_pending()
+  time.sleep(1)
