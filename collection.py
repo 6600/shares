19dd959793a661d82ec2270277834091db65ac29
PUGE <100284685@qq.com>
2019-04-25 00:24:17 +0800

    save

--- collection.py
+++ collection.py
@@ -107,15 +107,15 @@ def stop():
 
 def pack():
   startdir = "./history/temp"  #要压缩的文件夹路径
-  # file_news ='./history/' + time.strftime('%Y.%m.%d',time.localtime(time.time())) +'.zip' # 压缩后文件夹的名字
-  # z = zipfile.ZipFile(file_news,'w',zipfile.ZIP_DEFLATED) #参数一：文件夹名
-  # for dirpath, dirnames, filenames in os.walk(startdir):
-  #     fpath = dirpath.replace(startdir,'') #这一句很重要，不replace的话，就从根目录开始复制
-  #     fpath = fpath and fpath + os.sep or ''#这句话理解我也点郁闷，实现当前文件夹以及包含的所有文件的压缩
-  #     for filename in filenames:
-  #         z.write(os.path.join(dirpath, filename),fpath+filename)
-  # z.close()
-  # time.sleep(5)
+  file_news ='./history/' + time.strftime('%Y.%m.%d',time.localtime(time.time())) +'.zip' # 压缩后文件夹的名字
+  z = zipfile.ZipFile(file_news,'w',zipfile.ZIP_DEFLATED) #参数一：文件夹名
+  for dirpath, dirnames, filenames in os.walk(startdir):
+      fpath = dirpath.replace(startdir,'') #这一句很重要，不replace的话，就从根目录开始复制
+      fpath = fpath and fpath + os.sep or ''#这句话理解我也点郁闷，实现当前文件夹以及包含的所有文件的压缩
+      for filename in filenames:
+          z.write(os.path.join(dirpath, filename),fpath+filename)
+  z.close()
+  time.sleep(5)
   shutil.rmtree(startdir)
 
 def sendMessage():
