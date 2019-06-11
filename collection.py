dc437c8e7f1ce75127bea101f4f68f794ecc5dc3
PUGE <100284685@qq.com>
2019-06-11 23:28:14 +0800

    最近逐笔成交情况

--- collection.py
+++ collection.py
@@ -40,51 +40,113 @@ keepRun = False
 summary = ''
 # 数据生成时间
 timeData = ''
+# 经过分析的数据
 
+analyse = {}
+
+# 五秒交易量
+fiveSecondTradingVolume = {}
+
+# 读取上证列表
 szListFile = open('sz.json', 'r', encoding='utf-8').read()
 szList = json.loads(szListFile)
 
+# 读取配置
 config = json.load(open("config.json", encoding='utf-8'))
 weixin = Weixin(corpid=config['weixin']['corpid'], corpsecret=config['weixin']['corpsecret'])
+
+# 最近逐笔成交情况
+# [买卖情况, 时间间隔]
+def getDiveSecondTradingVolume (splitData):
+  # 排除不合法数据
+  if (splitData[28] == ''):
+    return [0, 0]
+  # 从原始数据中获取逐笔成交情况
+  transactionArr = splitData[28].split('|')
+
+  # 初步统计数据 买数 买量 卖数 卖量 时间间隔
+  firstData = [0, 0, 0, 0, 0]
+  # 遍历每一笔交易
+  for index in range(len(transactionArr)):
+    hand = transactionArr[index]
+
+    handArr = hand.split('/')
+
+    # 记录初始时间
+    if (index == 0):
+      firstData[4] = int(handArr[0].replace(':', ''))
+    
+    # 判断是否为结束时间
+    if (len(transactionArr) == index + 1):
+      firstData[4] = firstData[4] - int(handArr[0].replace(':', ''))
+
+    # 判断是买还是卖
+    if (handArr[3] == 'B'):
+      # 买单数量
+      firstData[0] += 1
+      # 买
+      firstData[1] += int(handArr[2])
+    else:
+      # 卖
+      # 卖单数量
+      firstData[2] += 1
+      firstData[3] += int(handArr[2])
+  
+  return [firstData[0] * firstData[1] - firstData[2] * firstData[3], firstData[4]]
+
+
+def getData (tempList):
+
+  listString = ','.join(tempList)
+  data = requests.get(url='http://qt.gtimg.cn/q=' + listString).text
+  sharesList = re.findall(r"=\"1\~(.+?)\";", data)
+  
+  # 去掉空数据
+  for sz in sharesList:
+    global summary
+    global timeData
+    global fiveSecondTradingVolume
+
+    splitData = sz.split('~')
+    # 股票id
+    stockID = splitData[1]
+    fiveSecondTradingVolume[stockID] = getDiveSecondTradingVolume(splitData)
+
+    if (timeData == ''):
+      timeData = splitData[29]
+    summary += sz.replace("~", ",") + '\n'
 
 
 # 存储数据
 def run():
-  
   global szList
   global keepRun
   while keepRun:
     global summary
     global timeData
+    global analyse
+    global fiveSecondTradingVolume
     summary = '股票名字,代码,当前价格,昨日收盘价,今日开盘价,成交的股票数,外盘,内盘,买一报价,买一数量,买二报价,买二数量,买三报价,买三数量,买四报价,买四数量,买五报价,买五数量,卖一报价,卖一数量,卖二报价,卖二数量,卖三报价,卖三数量,卖四报价,卖四数量,卖五报价,卖五数量,最近逐笔成交,时间,涨跌,涨跌%,今日最高价,今日最低价,价格/成交量(手)/成交额,成交量(手),成交金额,换手率(%),市盈率,最高,最低,振幅(%),流通市值(亿),总市值(亿),市净率,涨停价,跌停价,量比,委差,平均成本,PE(动)\n'
     # 每200个提交一次
+
+    # 当前等待获取数据列表
     tempList = []
     timeData = ''
-    def getData ():
-      listString = ','.join(tempList)
-      data = requests.get(url='http://qt.gtimg.cn/q=' + listString).text
-      sharesList = re.findall(r"=\"1\~(.+?)\";", data)
-      
-      # 去掉空数据
-      for sz in sharesList:
-        global summary
-        global timeData
-        if (timeData == ''):
-          timeData = sz.split('~')[29]
-        
-        summary += sz.replace("~",",") + '\n'
-
+    fiveSecondTradingVolume = {}
+    
+    # 统计每一股的数据
     for sz in szList:
       if len(tempList) >= 200:
-        getData()
+        getData(tempList)
         tempList = []
       tempList.append(sz['id'])
     # 处理尾巴
-    getData()
+    getData(tempList)
     file_path = './history/temp/' + timeData + '.csv'
     with open(file_path, mode='w', encoding='utf_8_sig') as file_obj:
       file_obj.write(summary)
     
+    print(fiveSecondTradingVolume)
     # 每5秒执行一次
     schedule.run_pending()
     time.sleep(5)
@@ -143,6 +205,8 @@ def sendMessage():
     "safe":0
   })
 
+start()
+print('程序正在等待指定时间运行!')
 # 指定时间运行
 schedule.every().day.at("09:25").do(start)
 schedule.every().day.at("11:35").do(stop)
